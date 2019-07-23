6a3cf67741cfca1b16fc30bff72c58dbd909b131
Oscar Pérez Fuentes <oscar@xmltravelgate.com>
2019-07-23 09:35:02 +0200

    wip

--- app/routers/slack_commands.py
+++ app/routers/slack_commands.py
@@ -1,5 +1,5 @@
 import logging
-import time
+import urllib.parse
 from fastapi import BackgroundTasks,APIRouter,HTTPException,Form
 from pydantic import BaseModel,Schema
 from app.common.config import Config
@@ -15,9 +15,9 @@ router = APIRouter()
 
 #https://api.slack.com/slash-commands#best_practices
 @router.post("/slack/commands", tags=["slack","commands"])
-async def post_event(*, background_tasks: BackgroundTasks):
+async def post_event(*, command: str = Form(...), background_tasks: BackgroundTasks):
    logger.info(f"POST Slack")
-   command_model = CommandModelIn( command="/insightsx", response_url="http://wwww.google.com" ) 
+   command_model = CommandModelIn( command=urllib.parse.unquote(command), response_url="http://wwww.google.com" ) 
    logger.info(f"Slack commands:[{command_model}]")
    if command_model.command == "/insightsx": 
       return CommandModelOut(text="Command received")
